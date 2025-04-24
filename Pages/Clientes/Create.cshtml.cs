2c5f983041a0b46d70709db64886bc38003d4d44
Piero <piero10.5.rodrigo@hotmail.com>
2025-04-24 11:33:09 -0500

    v2 del crud

--- Pages/Clientes/Create.cshtml.cs
+++ Pages/Clientes/Create.cshtml.cs
@@ -1,12 +1,28 @@
+using appweb1.Data;
+using appweb1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
     public class CreateModel : PageModel
-    {
-        public void OnGet()
-        {
+    {   
+
+        private readonly AppDbContext _ctx;
+
+        public CreateModel(AppDbContext ctx){
+            _ctx=ctx;
+        }
+
+        [BindProperty]
+
+        public Cliente Cliente {get;set;}
+        public void OnGet(){}
+
+        public IActionResult OnPost(){
+            _ctx.Clientes.Add(Cliente);
+            _ctx.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
--- obj/Debug/net9.0/appweb1.AssemblyInfo.cs
+++ obj/Debug/net9.0/appweb1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appweb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8d0987a690a42736576d2692fabd15f8bbd582")]
 [assembly: System.Reflection.AssemblyProductAttribute("appweb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appweb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
